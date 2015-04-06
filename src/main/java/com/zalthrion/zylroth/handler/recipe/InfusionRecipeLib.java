0ac93b58a2b4bc96eb5b98ef4a2ba9f08f0e7d5d
61352151511 <cassboys@hotmail.com>
2015-04-06 14:00:03 -0300

    Recipe Handler Work, Not 100% Done

--- src/main/java/com/zalthrion/zylroth/handler/recipe/InfusionRecipeLib.java
+++ src/main/java/com/zalthrion/zylroth/handler/recipe/InfusionRecipeLib.java
@@ -1,20 +1,37 @@
 package com.zalthrion.zylroth.handler.recipe;
 
+import java.util.ArrayList;
+
 import net.minecraft.item.ItemStack;
 
 public class InfusionRecipeLib {
 	private ItemStack input;
 	private ItemStack output;
-	private ItemStack[] infusionMaterials;
+	private int experience;
+	private ArrayList<ItemStack> infusionMaterials;
 	
 	/** Creates a new InfusionRecipe
 	 * @param in - ItemStack being infused
 	 * @param out - ItemStack after infusion
+	 * @param experience - Experience earned from performing this infusion.
 	 * @param infusingMaterials - Materials used to infuse */
-	public InfusionRecipeLib(ItemStack in, ItemStack out, ItemStack... infusionMaterials) {
+	public InfusionRecipeLib(ItemStack in, ItemStack out, int experience, ItemStack... infusionMaterials) {
 		this.input = in;
 		this.output = out;
-		this.infusionMaterials = infusionMaterials;
+		this.experience = experience;
+		this.infusionMaterials = new ArrayList<ItemStack>();
+		for (ItemStack stack : infusionMaterials) {
+			this.infusionMaterials.add(stack);
+		}
+	}
+	
+	@Deprecated public InfusionRecipeLib(ItemStack in, ItemStack out, ItemStack... infusionMaterials) {
+		this(in, out, 0, infusionMaterials);
+	}
+	
+	/** Returns amount of experience earned from performing this infusion **/
+	public int getExperience() {
+		return this.experience;
 	}
 	
 	/** Returns ItemStack after infusion **/
@@ -28,7 +45,11 @@ public class InfusionRecipeLib {
 	}
 	
 	/** Returns ItemStack's required to infuse this item **/
-	public ItemStack[] getInfusionMaterials() {
+	public ArrayList<ItemStack> getInfusionMaterials() {
 		return infusionMaterials;
+	}
+	
+	public InfusionRecipeLib copy() {
+		return new InfusionRecipeLib(this.input, this.output, this.experience, this.infusionMaterials.toArray(new ItemStack[this.infusionMaterials.size()]));
 	}
 }
