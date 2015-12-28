a5f5dcea62a719c15adf8cb9b96b0210e9d08e2f
61352151511 <cassboys@hotmail.com>
2015-12-27 21:26:49 -0400

    Stop EnderDragons from destroying Endirite Ore  Those damn griefers :dragon_face:

--- src/main/java/com/zalthrion/zylroth/block/ore/EndiriteOre.java
+++ src/main/java/com/zalthrion/zylroth/block/ore/EndiriteOre.java
@@ -4,7 +4,11 @@ import java.util.Random;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.item.Item;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 import com.zalthrion.zylroth.block.BlockBase;
 import com.zalthrion.zylroth.lib.ModItems;
@@ -29,5 +33,12 @@ public class EndiriteOre extends BlockBase {
 	
 	@Override public int quantityDropped(Random rand) {
 		return 2 + rand.nextInt(4);
+	}
+	
+	@Override public boolean canEntityDestroy(IBlockAccess world, BlockPos pos, Entity entity) {
+		if (entity instanceof EntityDragon) {
+			return false;
+		}
+		return true;
 	}
 }
