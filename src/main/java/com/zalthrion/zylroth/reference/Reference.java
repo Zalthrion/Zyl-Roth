aba7b59ca749e940f831d299e76169a0689d738c
61352151511 <cassboys@hotmail.com>
2015-12-30 22:31:45 -0400

    1.8.9

--- src/main/java/com/zalthrion/zylroth/reference/Reference.java
+++ src/main/java/com/zalthrion/zylroth/reference/Reference.java
@@ -3,7 +3,7 @@ package com.zalthrion.zylroth.reference;
 public class Reference {
 	public static final String MOD_ID = "Zylroth";
 	public static final String MOD_NAME = "Zyl'roth";
-	public static final String MINECRAFT_VERSION = "1.8.8";
+	public static final String MINECRAFT_VERSION = "1.8.9";
 	public static final String VERSION = "1.0.0";
 	public static final String CLIENT_PROXY = "com.zalthrion.zylroth.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.zalthrion.zylroth.proxy.ServerProxy";
