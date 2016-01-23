9a3ec5908e75416a6069a54fb9c3b3ab50766035
Zalthrion <revenge-of-the-nerds@live.com>
2016-01-23 15:52:52 -0500

    New Animal: Boar
    Hostile on Attack
    
    Max Health: 15
    Speed: 0.25
    Attack Damage: 5
    Follow Range: 15
    
    Follow Item: Potato, Baked Potato
    Mate Item: Potato
    Child Model: Yes

--- src/main/java/com/zalthrion/zylroth/entity/EntityBoar.java
+++ src/main/java/com/zalthrion/zylroth/entity/EntityBoar.java
@@ -1,32 +1,95 @@
 package com.zalthrion.zylroth.entity;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
-import net.minecraft.entity.ai.EntityAIPanic;
+import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
-import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
-public class EntityBoar extends EntityAnimal {
+public class EntityBoar extends EntityHostileAnimal {
 	public EntityBoar(World world) {
 		super(world);
 		this.setSize(1F, 1F);
-		this.tasks.addTask(0, new EntityAIPanic(this, 1.25D));
-		this.tasks.addTask(1, new EntityAIWander(this, 1.0D));
-		this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
-		this.tasks.addTask(3, new EntityAILookIdle(this));
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.2D, false));
+        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.2D, Items.potato, false));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.2D, Items.baked_potato, false));
+        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.1D));
+		this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+		this.tasks.addTask(7, new EntityAILookIdle(this));
+		this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, true));
 	}
 	
-	@Override protected void applyEntityAttributes() {
+	public boolean isAIEnabled() {
+		return true;
+	}
+	
+	@Override
+	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(10.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(15.0D);
 		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.25D);
+		if (!this.worldObj.isRemote && !(this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL))
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(5.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(15.0D);
+		
 	}
 	
-	@Override public EntityAgeable createChild(EntityAgeable ageable) {
+	@Override
+	public EntityAgeable createChild(EntityAgeable ageable) {
 		return new EntityBoar(this.worldObj);
+	}
+	
+	protected Entity findPlayerToAttack() {
+		EntityPlayer entityplayer = this.worldObj.getClosestVulnerablePlayerToEntity(this, 16.0D);
+		return entityplayer != null && this.canEntityBeSeen(entityplayer) ? entityplayer : null;
+	}
+	
+	/**
+	 * Returns the sound this mob makes while it's alive.
+	 */
+	protected String getLivingSound() {
+		return "mob.pig.say";
+	}
+	
+	/**
+	 * Returns the sound this mob makes when it is hurt.
+	 */
+	protected String getHurtSound() {
+		return "mob.pig.say";
+	}
+	
+	/**
+	 * Returns the sound this mob makes on death.
+	 */
+	protected String getDeathSound() {
+		return "mob.pig.death";
+	}
+	
+	protected void playStepSound(int x, int y, int z, Block blockIn) {
+		this.playSound("mob.pig.step", 0.15F, 1.0F);
+	}
+	
+	/**
+	 * Checks if the parameter is an item which this animal can be fed to breed it (wheat, carrots or seeds depending on
+	 * the animal type)
+	 */
+	public boolean isBreedingItem(ItemStack p_70877_1_) {
+		return p_70877_1_ != null && p_70877_1_.getItem() == Items.potato;
 	}
 }
--- src/main/java/com/zalthrion/zylroth/model/entity/ModelBoar.java
+++ src/main/java/com/zalthrion/zylroth/model/entity/ModelBoar.java
@@ -1,5 +1,7 @@
 package com.zalthrion.zylroth.model.entity;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
@@ -10,75 +12,98 @@ import net.minecraft.util.MathHelper;
  * Created using Tabula 4.1.1
  */
 public class ModelBoar extends ModelBase {
-    public ModelRenderer Body;
-    public ModelRenderer BackLeftLeg;
-    public ModelRenderer FrontLeftLeg;
-    public ModelRenderer BackRightLeg;
-    public ModelRenderer FrontRightLeg;
-    public ModelRenderer Head;
-    public ModelRenderer LeftTuskBase;
-    public ModelRenderer LeftTusk;
-    public ModelRenderer RightTuskBase;
-    public ModelRenderer RightTusk;
-    public ModelRenderer Snout;
-
-    public ModelBoar() {
-        this.textureWidth = 64;
-        this.textureHeight = 32;
-        this.Head = new ModelRenderer(this, 0, 0);
-        this.Head.setRotationPoint(-0.5F, 13.5F, -6.0F);
-        this.Head.addBox(-4.0F, -4.0F, -8.0F, 9, 7, 8, 0.0F);
-        this.FrontRightLeg = new ModelRenderer(this, 0, 16);
-        this.FrontRightLeg.setRotationPoint(-3.0F, 18.0F, -5.0F);
-        this.FrontRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F);
-        this.FrontLeftLeg = new ModelRenderer(this, 0, 16);
-        this.FrontLeftLeg.setRotationPoint(3.0F, 18.0F, -5.0F);
-        this.FrontLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F);
-        this.BackLeftLeg = new ModelRenderer(this, 0, 16);
-        this.BackLeftLeg.setRotationPoint(3.0F, 18.0F, 5.5F);
-        this.BackLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F);
-        this.RightTuskBase = new ModelRenderer(this, 0, 0);
-        this.RightTuskBase.setRotationPoint(-2.0F, 0.7F, -9.3F);
-        this.RightTuskBase.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
-        this.Snout = new ModelRenderer(this, 16, 16);
-        this.Snout.setRotationPoint(0.5F, -1.0F, 0.0F);
-        this.Snout.addBox(-2.0F, 0.0F, -9.0F, 4, 3, 1, 0.0F);
-        this.LeftTuskBase = new ModelRenderer(this, 0, 0);
-        this.LeftTuskBase.setRotationPoint(2.0F, 0.7F, -9.3F);
-        this.LeftTuskBase.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
-        this.BackRightLeg = new ModelRenderer(this, 0, 16);
-        this.BackRightLeg.setRotationPoint(-3.0F, 18.0F, 5.5F);
-        this.BackRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 6, 4, 0.0F);
-        this.Body = new ModelRenderer(this, 28, 8);
-        this.Body.setRotationPoint(0.0F, 11.0F, 2.0F);
-        this.Body.addBox(-5.0F, -10.0F, -7.0F, 10, 16, 8, 0.0F);
-        this.setRotateAngle(Body, 1.5707963267948966F, 0.0F, 0.0F);
-        this.RightTusk = new ModelRenderer(this, 0, 0);
-        this.RightTusk.setRotationPoint(-2.0F, 1.73F, -9.35F);
-        this.RightTusk.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
-        this.setRotateAngle(RightTusk, 2.280098134805392F, 0.0F, 0.0F);
-        this.LeftTusk = new ModelRenderer(this, 0, 0);
-        this.LeftTusk.setRotationPoint(2.0F, 1.73F, -9.35F);
-        this.LeftTusk.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
-        this.setRotateAngle(LeftTusk, 2.280098134805392F, 0.0F, 0.0F);
-        this.Head.addChild(this.RightTuskBase);
-        this.Head.addChild(this.Snout);
-        this.Head.addChild(this.LeftTuskBase);
-        this.Head.addChild(this.RightTusk);
-        this.Head.addChild(this.LeftTusk);
-    }
-
-    @Override
-    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
-        this.Head.render(f5);
-        this.FrontRightLeg.render(f5);
-        this.FrontLeftLeg.render(f5);
-        this.BackLeftLeg.render(f5);
-        this.BackRightLeg.render(f5);
-        this.Body.render(f5);
-    }
-    
-	@Override public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity entityIn) {
+	public ModelRenderer Body;
+	public ModelRenderer BackLeftLeg;
+	public ModelRenderer FrontLeftLeg;
+	public ModelRenderer BackRightLeg;
+	public ModelRenderer FrontRightLeg;
+	public ModelRenderer Head;
+	public ModelRenderer LeftTuskBase;
+	public ModelRenderer LeftTusk;
+	public ModelRenderer RightTuskBase;
+	public ModelRenderer RightTusk;
+	public ModelRenderer Snout;
+	
+    protected float field_78145_g = 8.0F;
+    protected float field_78151_h = 4.0F;
+	
+	public ModelBoar() {
+		this.textureWidth = 64;
+		this.textureHeight = 32;
+		this.BackRightLeg = new ModelRenderer(this, 0, 16);
+		this.BackRightLeg.setRotationPoint(-3.0F, 17.0F, 5.5F);
+		this.BackRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 7, 4, 0.0F);
+		this.Body = new ModelRenderer(this, 28, 8);
+		this.Body.setRotationPoint(0.0F, 11.0F, 2.0F);
+		this.Body.addBox(-5.0F, -10.0F, -7.0F, 10, 16, 8, 0.0F);
+		this.setRotateAngle(Body, 1.5707963267948966F, 0.0F, 0.0F);
+		this.RightTusk = new ModelRenderer(this, 0, 0);
+		this.RightTusk.setRotationPoint(-2.0F, 1.73F, -9.35F);
+		this.RightTusk.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
+		this.setRotateAngle(RightTusk, 2.280098134805392F, 0.0F, 0.0F);
+		this.BackLeftLeg = new ModelRenderer(this, 0, 16);
+		this.BackLeftLeg.setRotationPoint(3.0F, 17.0F, 5.5F);
+		this.BackLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 7, 4, 0.0F);
+		this.RightTuskBase = new ModelRenderer(this, 0, 0);
+		this.RightTuskBase.setRotationPoint(-2.0F, 0.7F, -9.3F);
+		this.RightTuskBase.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
+		this.FrontLeftLeg = new ModelRenderer(this, 0, 16);
+		this.FrontLeftLeg.setRotationPoint(3.0F, 17.0F, -5.0F);
+		this.FrontLeftLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 7, 4, 0.0F);
+		this.Snout = new ModelRenderer(this, 16, 16);
+		this.Snout.setRotationPoint(0.5F, -1.0F, 0.0F);
+		this.Snout.addBox(-2.0F, 0.0F, -9.0F, 4, 3, 1, 0.0F);
+		this.LeftTusk = new ModelRenderer(this, 0, 0);
+		this.LeftTusk.setRotationPoint(2.0F, 1.73F, -9.35F);
+		this.LeftTusk.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
+		this.setRotateAngle(LeftTusk, 2.280098134805392F, 0.0F, 0.0F);
+		this.FrontRightLeg = new ModelRenderer(this, 0, 16);
+		this.FrontRightLeg.setRotationPoint(-3.0F, 17.0F, -5.0F);
+		this.FrontRightLeg.addBox(-2.0F, 0.0F, -2.0F, 4, 7, 4, 0.0F);
+		this.LeftTuskBase = new ModelRenderer(this, 0, 0);
+		this.LeftTuskBase.setRotationPoint(2.0F, 0.7F, -9.3F);
+		this.LeftTuskBase.addBox(0.0F, 0.0F, 0.0F, 1, 1, 2, 0.0F);
+		this.Head = new ModelRenderer(this, 0, 0);
+		this.Head.setRotationPoint(-0.5F, 13.5F, -6.0F);
+		this.Head.addBox(-4.0F, -4.0F, -8.0F, 9, 7, 8, 0.0F);
+		this.Head.addChild(this.RightTusk);
+		this.Head.addChild(this.RightTuskBase);
+		this.Head.addChild(this.Snout);
+		this.Head.addChild(this.LeftTusk);
+		this.Head.addChild(this.LeftTuskBase);
+	}
+	
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		this.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+		if (this.isChild) {
+			float f6 = 2.0F;
+			GL11.glPushMatrix();
+			GL11.glScalef(1.5F / f6, 1.5F / f6, 1.5F / f6);
+			GL11.glTranslatef(0.0F, field_78145_g * f5, field_78151_h * f5);
+			this.Head.render(f5);
+			GL11.glPopMatrix();
+			GL11.glPushMatrix();
+			GL11.glScalef(1.0F / f6, 1.0F / f6, 1.0F / f6);
+			GL11.glTranslatef(0.0F, 24.0F * f5, 0.0F);
+			this.FrontRightLeg.render(f5);
+			this.FrontLeftLeg.render(f5);
+			this.BackLeftLeg.render(f5);
+			this.BackRightLeg.render(f5);
+			this.Body.render(f5);
+			GL11.glPopMatrix();
+		} else {
+			this.Head.render(f5);
+			this.FrontRightLeg.render(f5);
+			this.FrontLeftLeg.render(f5);
+			this.BackLeftLeg.render(f5);
+			this.BackRightLeg.render(f5);
+			this.Body.render(f5);
+		}
+	}
+	
+	@Override
+	public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity entityIn) {
 		this.Head.rotateAngleX = p_78087_5_ / (180F / (float) Math.PI);
 		this.Head.rotateAngleY = p_78087_4_ / (180F / (float) Math.PI);
 		this.Body.rotateAngleX = ((float) Math.PI / 2F);
@@ -87,13 +112,13 @@ public class ModelBoar extends ModelBase {
 		this.BackLeftLeg.rotateAngleX = MathHelper.cos(p_78087_1_ * 0.6662F + (float) Math.PI) * 1.4F * p_78087_2_;
 		this.BackRightLeg.rotateAngleX = MathHelper.cos(p_78087_1_ * 0.6662F) * 1.4F * p_78087_2_;
 	}
-    
-    /**
-     * This is a helper function from Tabula to set the rotation of model parts
-     */
-    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
-        modelRenderer.rotateAngleX = x;
-        modelRenderer.rotateAngleY = y;
-        modelRenderer.rotateAngleZ = z;
-    }
+	
+	/**
+	 * This is a helper function from Tabula to set the rotation of model parts
+	 */
+	public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
 }
